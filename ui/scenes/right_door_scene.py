6234738dd54f7765a5c4d33aab3add6f65e1b84c
MrDe4olT <timkom2280@gmail.com>
2026-06-18 22:33:18 +0500

    Added right corridor. Player can peak into a corridor by pressing on black rect at the door

--- ui/scenes/right_door_scene.py
+++ ui/scenes/right_door_scene.py
@@ -11,16 +11,24 @@ class RightDoorScene(SceneBase):
         self.door_opened_image = self.game.assets.images["door_opened"]
         self.door_closed_image = self.game.assets.images["door_closed"]
         self.exit_button_image = self.game.assets.images["exit_btn"]
+
         self.door_hold_rect = pygame.Rect(617, 0, 1177, 1080)
+        self.peek_rect = pygame.Rect(115, 0, 505, 1080)
 
         door_button_width = self.exit_button_image.get_width()
         door_button_height = self.exit_button_image.get_height()
-        self.exit_button_image = pygame.transform.smoothscale(self.exit_button_image, (door_button_width, door_button_height))
+        self.exit_button_image = pygame.transform.smoothscale(
+            self.exit_button_image,
+            (door_button_width, door_button_height),
+        )
         self.exit_button_rect = self.exit_button_image.get_rect(midbottom=(960, 1080))
 
     def handle_event(self, event) -> None:
         """Handle scene events."""
-        pass
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if self.peek_rect.collidepoint(event.pos):
+                from ui.scenes.right_corridor_scene import RightCorridorScene
+                self.game.change_scene(RightCorridorScene(self.game))
 
     def update(self, dt: float) -> None:
         """Update right door state while the player is holding the mouse button."""
@@ -39,7 +47,6 @@ class RightDoorScene(SceneBase):
         session.office.right_door_closed = (
             mouse_pressed and self.door_hold_rect.collidepoint(mouse_pos)
         )
-
         session.office.set_monitor("right_door")
         session.update(dt, False)
 
@@ -50,6 +57,7 @@ class RightDoorScene(SceneBase):
         else:
             screen.blit(self.door_opened_image, (0, 0))
 
-        """pygame.draw.rect(screen, (255, 0, 0), self.door_hold_rect, 2)"""
+        # pygame.draw.rect(screen, (255, 0, 0), self.door_hold_rect, 2)
+        pygame.draw.rect(screen, (0, 255, 0), self.peek_rect, 2)
 
         screen.blit(self.exit_button_image, self.exit_button_rect.topleft)

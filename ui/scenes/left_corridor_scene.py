fd7200c31bb69f000266760c847a9f0376c48729
MrDe4olT <timkom2280@gmail.com>
2026-06-18 23:34:12 +0500

    Fixed problem with death from anxiety in corridors. Added first stage for boss

--- ui/scenes/left_corridor_scene.py
+++ ui/scenes/left_corridor_scene.py
@@ -13,20 +13,23 @@ class LeftCorridorScene(SceneBase):
 
         door_button_width = self.exit_button_image.get_width()
         door_button_height = self.exit_button_image.get_height()
-        self.exit_button_image = pygame.transform.smoothscale(self.exit_button_image, (door_button_width, door_button_height))
+        self.exit_button_image = pygame.transform.smoothscale(
+            self.exit_button_image,
+            (door_button_width, door_button_height),
+        )
         self.exit_button_image = pygame.transform.rotate(self.exit_button_image, 270)
         self.exit_button_rect = self.exit_button_image.get_rect(midleft=(560, 540))
 
     def handle_event(self, event) -> None:
         """Handle scene events."""
-
+        pass
 
     def update(self, dt: float) -> None:
         """Update session while player is looking at the left corridor."""
         session = self.game.session
         if session is None:
             return
-        
+
         mouse_pos = pygame.mouse.get_pos()
 
         if self.exit_button_rect.collidepoint(mouse_pos):
@@ -36,6 +39,21 @@ class LeftCorridorScene(SceneBase):
 
         session.office.set_monitor("left_corridor")
         session.update(dt, False)
+
+        if session.finished:
+            if session.result == "survived":
+                self.game.day = self.game.save_manager.next_day()
+                self.game.day_config = self.game.difficulty_manager.get_day_config(self.game.day)
+
+                if self.game.save_manager.is_game_completed():
+                    from ui.scenes.win_scene import WinScene
+                    self.game.change_scene(WinScene(self.game))
+                else:
+                    from ui.scenes.game_over_scene import GameOverScene
+                    self.game.change_scene(GameOverScene(self.game, session.result))
+            else:
+                from ui.scenes.game_over_scene import GameOverScene
+                self.game.change_scene(GameOverScene(self.game, session.result))
 
     def draw(self, screen) -> None:
         """Draw the left corridor and exit button."""
--- ui/scenes/right_corridor_scene.py
+++ ui/scenes/right_corridor_scene.py
@@ -13,28 +13,47 @@ class RightCorridorScene(SceneBase):
 
         door_button_width = self.exit_button_image.get_width()
         door_button_height = self.exit_button_image.get_height()
-        self.exit_button_image = pygame.transform.smoothscale(self.exit_button_image, (door_button_width, door_button_height))
+        self.exit_button_image = pygame.transform.smoothscale(
+            self.exit_button_image,
+            (door_button_width, door_button_height),
+        )
         self.exit_button_image = pygame.transform.rotate(self.exit_button_image, 90)
         self.exit_button_rect = self.exit_button_image.get_rect(midright=(1360, 540))
 
     def handle_event(self, event) -> None:
         """Handle scene events."""
+        pass
 
     def update(self, dt: float) -> None:
         """Update session while player is looking at the right corridor."""
         session = self.game.session
         if session is None:
             return
-        
+
         mouse_pos = pygame.mouse.get_pos()
 
         if self.exit_button_rect.collidepoint(mouse_pos):
-            from ui.scenes.left_door_scene import LeftDoorScene
-            self.game.change_scene(LeftDoorScene(self.game))
+            from ui.scenes.right_door_scene import RightDoorScene
+            self.game.change_scene(RightDoorScene(self.game))
             return
 
         session.office.set_monitor("right_corridor")
         session.update(dt, False)
+
+        if session.finished:
+            if session.result == "survived":
+                self.game.day = self.game.save_manager.next_day()
+                self.game.day_config = self.game.difficulty_manager.get_day_config(self.game.day)
+
+                if self.game.save_manager.is_game_completed():
+                    from ui.scenes.win_scene import WinScene
+                    self.game.change_scene(WinScene(self.game))
+                else:
+                    from ui.scenes.game_over_scene import GameOverScene
+                    self.game.change_scene(GameOverScene(self.game, session.result))
+            else:
+                from ui.scenes.game_over_scene import GameOverScene
+                self.game.change_scene(GameOverScene(self.game, session.result))
 
     def draw(self, screen) -> None:
         """Draw the right corridor and exit button."""
